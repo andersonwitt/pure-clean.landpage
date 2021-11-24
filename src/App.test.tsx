1f4ced4342f852da2d4fbfb17440ec15cffbfeb1
Anderson <andersonrw16@hotmail.com>
2021-11-23 21:23:49 -0300

    FIRST COMMIT

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Pure Clean Initial Blank Page/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pure Clean Initial Blank Page</h1>
+    </div>
+  );
+}
+
+export default App;
